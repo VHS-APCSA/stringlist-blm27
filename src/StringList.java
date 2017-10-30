c2027096cc51fbdda9e6585e1984dac0427df569
bob <bob@APCSA>
2017-10-30 14:43:28 -0700

    added toString

--- src/StringList.java
+++ src/StringList.java
@@ -1,8 +1,34 @@
 import java.util.ArrayList;
 public class StringList 
 {
-	private ArrayList<String> strings;
+	private ArrayList<String> list;
+	public StringList()
 	{
-		
+		list = new ArrayList<String>();
+	}
+	public void addWord(String word)
+	{
+		list.add(word);
+	}
+	public String toString()
+	{
+		String str = "";
+		for(String strings : list)
+		{
+			str += strings + "\n";
+		}
+		return str;
+	}
+	public String reverse(String word)
+	{
+		String reverse = "";
+		int index = 0;
+		while(index < word.length())
+		{
+			System.out.println(word.substring(index +1, index));
+			
+			index++;
+		}
+		return reverse;
 	}
 }
